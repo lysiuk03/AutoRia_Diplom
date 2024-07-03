c1ca7a33e8155960611dbde9fe069a76934b844c
lovisnice <vovaprorok29@gmail.com>
2024-07-03 21:34:50 +0300

    підключив базу даних, використав розширення Npgsql.EntityFrameworkCore.PostgreSQL, успішно створена перша міграція, внесено зміни до бд

--- WebBack/WebBack/Program.cs
+++ WebBack/WebBack/Program.cs
@@ -1,15 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebBack.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//string connectionString = builder.Configuration.GetConnectionString("ConnectionSqlite");
+
+
+//builder.Services.AddDbContext(connection);
+
+
+//builder.Services.AddDbContext<DataContext>(options=>
+//options.UseSqlite("postgresql://ep-yellow-dream-a26ddv09.eu-central-1.aws.neon.tech:5432/global"));
+
+
+builder.Services.AddAuthorization();
+//builder.Services.AddAuthorization().AddCookiePolicy(IdentityConstants.ApplicationScheme);
+
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
